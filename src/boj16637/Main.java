9be6d1a30a2d8ced7c8b5584c8245c5674e31ccd
lee-dooghong <lovelyldh@naver.com>
2024-06-17 18:04:12 +0900

    uploadTest

---
+++ src/boj16637/Main.java
@@ -0,0 +1,19 @@
+package boj16637;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		new Main().solution();
+	}
+	
+	public void solution () {
+		pl("hello world");
+	}
+	
+	public void pl(Object o) {
+		System.out.println(o);
+	}
+
+}
